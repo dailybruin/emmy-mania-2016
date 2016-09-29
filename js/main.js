5fcbf3f6ade564042f1a868d735bf4116cd06c0d
Paulina Lei <paulina.y.lei@gmail.com>
2016-09-29 01:30:16 -0700

    handlebars + design changes + some formatting

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
-$(document).ready(function() {
+function articles() {
   $.ajax({
     dataType: "json",
     url: "https://spreadsheets.google.com/feeds/list/1WrNJOuYK1NtCGonvwEpkIUF6XIBAZGw6xmKHuGKRn9Y/od6/public/values?alt=json",
     success: function(data) {
       data = data.feed.entry;
       console.log(data);
+      var source = $("#handlebars-articles").html();
+      var template = Handlebars.compile(source);
+
+      var html = template(data[0]);
+      $("#emmy-mania-week-1").replaceWith(html);
     }
   });
+}
+
+$(document).ready(function() {
 });
